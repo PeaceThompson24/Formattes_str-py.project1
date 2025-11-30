f387e0b696da82974fd2592fb9a16962868bafa9
PeaceThompson24 <bapthompson54@gmail.com>
2025-11-30 14:55:29 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,7 @@
+# This code would be about formatted string and how to use it. It shows how to use the f string. we would use variables and expressions in the string 
+name = 'Peace Thompson'
+age = "18"
+department = "Computer Science"
+course_code ="SEN 201" 
+print(f'My name is {name} and i am {age} years old. I am a {department} student and i enjoy studying {course_code}') 
+# This is a formatted string text that would print out the variables of the students.
